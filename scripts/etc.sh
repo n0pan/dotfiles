d5a978eb96521bc976b59f2d396a85cc85ac1c16
LQ Hoang <longquanhp@gmail.com>
2019-09-10 07:07:23 -0400

    d

--- scripts/etc.sh
+++ scripts/etc.sh
@@ -10,3 +10,11 @@ git clone https://github.com/ToxicFrog/Ligaturizer.git --recurse-submodules
 # Install meteor
 curl https://install.meteor.com/ | sh
 
+# Clone Spicetify Theme
+git clone https://github.com/morpheusthewhite/spicetify-themes.git ~/spicetify-themes
+
+cd ~/spicetify-themes
+cp ./ ~/spicetify_data/Themes/
+
+spicetify config current_theme Arc-Dark
+spicetify backup apply
