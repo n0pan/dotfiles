e000af54c67c3983d9a4e35f3225f65cc9fe8c78
LQ Hoang <longquanhp@gmail.com>
2019-09-21 16:58:58 -0400

    updated ligaturizer path

--- scripts/etc.sh
+++ scripts/etc.sh
@@ -5,7 +5,7 @@ npm install neovim
 pip3 install --user neovim
 
 # Clone Ligaturizer
-git clone https://github.com/ToxicFrog/Ligaturizer.git --recurse-submodules
+git clone https://github.com/ToxicFrog/Ligaturizer.git --recurse-submodules ~/Ligaturizer
 
 # Install meteor
 curl https://install.meteor.com/ | sh
