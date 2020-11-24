fd77d113a250385202b6fd273ef02fc36ad7c6e1
LQ Hoang <longquanhp@gmail.com>
2020-11-23 21:51:32 -0500

    removed time from p10k

--- p10k/.p10k.zsh
+++ p10k/.p10k.zsh
@@ -64,7 +64,7 @@
       # vpn_ip                # virtual private network indicator
       ram                   # free RAM
       # load                  # CPU load
-      time                    # current time
+      # time                    # current time
       # public_ip             # public IP address
       # proxy                 # system-wide http/https/ftp proxy
       # battery               # internal battery
