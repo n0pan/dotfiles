41c73372e150de1c0cc593004e09118d917b022d
LQ Hoang <longquanhp@gmail.com>
2021-07-04 23:22:30 -0400

    fixed brew

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -4,10 +4,9 @@
 echo "Installing homebrew"
 
 which -s brew
-if [[ $? != 0 ]] ; then
-  yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  echo "Homebrew already installed ..."
+if [ "$?" -ne 0 ]; then
+   echo "No Homebrew found. Installing now..."
+   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update
