5d6cc8b9ee5b80cb4b56386f4aeb76943bf3d3d3
LQ Hoang <longquanhp@gmail.com>
2022-01-03 14:02:22 -0500

    deleted licensed fonts

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,14 +1,12 @@
 echo "INSTALLING FONTS"
 echo "----------------"
-echo "1) Installing Pragamata Pro"
-cp -R ~/dotfiles/fonts/Pragamata\ Pro/*.ttf /Library/Fonts
-echo "Done!"
-echo "2) Installing SF Mono"
+echo "1) Installing SF Mono"
 cp /Applications/Utilities/Terminal.app/Contents/Resources/Fonts/*.otf ~/Library/Fonts/ 
 echo "3) Installing fonts from cask"
 brew install --cask font-hack-nerd-font
 brew install --cask font-cascadia
 brew install --cask font-jetbrains-mono
 brew install --cask font-fira-code-nerd-font
+brew install --cask font-iosevka
 
 echo "Done!"
