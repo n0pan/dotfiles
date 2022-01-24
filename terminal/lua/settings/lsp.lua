517c537fa0b8f0afec8cb8b825fb92a42eb52b4e
LQ Hoang <longquanhp@gmail.com>
2022-01-23 23:52:00 -0500

    lsp: fixed go to definition

--- terminal/lua/settings/lsp.lua
+++ terminal/lua/settings/lsp.lua
@@ -1,7 +1,6 @@
 local servers = require("utils.lsp_servers")
 local lsp_installer = require("nvim-lsp-installer")
 
-
 lsp_installer.settings({
   ui = {
     icons = {
@@ -27,24 +26,20 @@ lsp_installer.on_server_ready(function(server)
   vim.cmd([[ do User LspAttach Buffers ]])
 end)
 
--- LSP --
 local attach_options = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", attach_options)
-local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>jd", "<cmd>lua vim.lsp.buf.definition()<CR>", attach_options)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>td", "<cmd>lua vim.lsp.buf.type_definition()<CR>", attach_options)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", attach_options)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", attach_options)
-end
 
+vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", attach_options)
+vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>lua vim.lsp.buf.declaration()<CR>', attach_options)
+vim.api.nvim_set_keymap("n", "<leader>jd", "<cmd>lua vim.lsp.buf.definition()<CR>", attach_options)
+vim.api.nvim_set_keymap("n", "<leader>td", "<cmd>lua vim.lsp.buf.type_definition()<CR>", attach_options)
+vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", attach_options)
+vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", attach_options)
+
+-- check and automatically install servers
 for _, lsp_name in pairs(servers) do
   local server_is_found, server = lsp_installer.get_server(lsp_name)
   if server_is_found and not server:is_installed() then
     print("Installing " .. lsp_name .. "...")
     server:install()
-  else require("lspconfig")[lsp_name].setup({
-    on_attach = on_attach
-  })
   end
 end
