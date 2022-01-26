cf3bf39943c387353e8386c6cb62cad2fc683443
LQ Hoang-Pham <longquanhp@gmail.com>
2022-01-26 01:05:45 -0500

    indent_lines: removed space char

--- neovim/lua/plugins/indent_lines.lua
+++ neovim/lua/plugins/indent_lines.lua
@@ -1,9 +1,9 @@
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("space:⋅")
 
 require("indent_blankline").setup({
   show_end_of_line = false,
-  space_char_blankline = " ",
+  -- space_char_blankline = " ",
   indentLine_enabled = 1,
   show_trailing_blankline_indent = false,
   show_first_indent_level = false,
