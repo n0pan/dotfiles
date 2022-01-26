d826dc153427447d4a85c83cf39abcd3a269d485
LQ Hoang-Pham <longquanhp@gmail.com>
2022-01-26 00:51:39 -0500

    git: don't activate blame automatically

--- neovim/lua/plugins/git.lua
+++ neovim/lua/plugins/git.lua
@@ -1,6 +1,6 @@
 require("gitsigns").setup({
   signcolumn = true,
-  current_line_blame = true,
+  current_line_blame = false,
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = "eol",
