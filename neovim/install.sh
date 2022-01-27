b71b7d77d7a75a9e97983838cbf85e2ff5fbc7ad
LQ Hoang-Pham <longquanhp@gmail.com>
2022-01-26 19:45:08 -0500

    vim/install: create directory after deleting

--- neovim/install.sh
+++ neovim/install.sh
@@ -5,6 +5,7 @@ rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
 rm -rf ~/.cache/nvim
 
+mkdir ~/.config/nvim
 ln -sf ~/dotfiles/neovim/init.lua ~/.config/nvim/init.lua
 ln -sf ~/dotfiles/neovim/lua ~/.config/nvim/lua
 
