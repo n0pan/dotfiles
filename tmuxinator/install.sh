e1bf6b48ed0ec9bcbcad7afbe821d8d1f75a289d
LQ Hoang-Pham <longquanhp@gmail.com>
2022-02-07 11:18:59 -0500

    chore(tmuxinator): fixed path for tmuxinator install

--- tmuxinator/install.sh
+++ tmuxinator/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "installing tmuxinator"
-ln -sf ./tmuxinator.zsh ~/.bin/tmuxinator.zsh
+ln -sf ~/dotfiles/tmuxinator/tmuxinator.zsh ~/.bin/tmuxinator.zsh
 ln -sf ~/dotfiles/tmuxinator ~/.config
 
 echo "done!"
