d87138eac86b41ee1c71f0d2ad4f163d0b5b748f
LQ Hoang-Pham <longquanhp@gmail.com>
2026-07-01 16:08:57 -0400

    chore: use kanawaga dragon theme

--- neovim/lua/settings/colorscheme.lua
+++ neovim/lua/settings/colorscheme.lua
@@ -1,33 +1,3 @@
--- require('nordic').setup({
---   on_palette = function(palette) end,
---   after_palette = function(palette) end,
---   on_highlight = function(highlights, palette) end,
---   bold_keywords = true,
---   italic_comments = true,
---   transparent = {
---     bg = true,
---     float = false,
---   },
---   bright_border = false,
---   reduced_blue = true,
---   swap_backgrounds = false,
---   cursorline = {
---     bold = false,
---     bold_number = true,
---     theme = 'light',
---     blend = 0.15,
---   },
---   telescope = {
---     style = 'flat',
---   },
---   leap = {
---     dim_backdrop = false,
---   },
---   ts_context = {
---     dark_background = true,
---   },
--- })
-
 -- -- catppuccin --
 -- require("catppuccin").setup({
 --   flavour = "mocha",
@@ -102,7 +72,7 @@ require('kanagawa').setup({
   end,
   theme = "dragon",  -- Load "wave" theme
   background = {   -- map the value of 'background' option to a theme
-    dark = "wave", -- try "dragon" !
+    dark = "dragon", -- try "dragon" !
     light = "lotus"
   },
 })
