08950295d733494155c9ade1a547df8fb1426fff
LQ Hoang-Pham <longquanhp@gmail.com>
2026-08-04 11:26:17 -0400

    chore: update setup

--- neovim/lua/plugins/smear.lua
+++ neovim/lua/plugins/smear.lua
@@ -4,17 +4,7 @@ if not setup then
 end
 
 smear.setup({
-  smear_between_buffers = true,
-  smear_between_neighbor_lines = true,
-  scroll_buffer_space = true,
-  legacy_computing_symbols_support = false,
-  smear_insert_mode = false,
-  stiffness = 0.8,                      -- 0.6      [0, 1]
-  matrix_pixel_threshold = 0.5,
-  stiffness_insert_mode = 0.5,          -- 0.5      [0, 1]
+  never_draw_over_target = false,
+  stiffness = 0.5,                      -- 0.6      [0, 1]
   trailing_stiffness = 0.45,            -- 0.45     [0, 1]
-  trailing_stiffness_insert_mode = 0.5, -- 0.5      [0, 1]
-  damping = 0.85,                       -- 0.85     [0, 1]
-  damping_insert_mode = 0.9,            -- 0.9      [0, 1]
-  distance_stop_animating = 0.4,        -- 0.1      > 0
 })
